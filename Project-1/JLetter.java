083ee40259243459c94a09a7cd4b26e8cc3dbca1
finnagainn <35272159+finnagainn@users.noreply.github.com>
2019-12-16 00:31:22 -0600

    Add files via upload

---
+++ Project-1/JLetter.java
@@ -0,0 +1,29 @@
+/** 
+ * Prints a block letter made of "JAVA".
+ *
+ * Project 1
+ * @author Kaffeein Bellamy
+ * @version 28/8/2019
+ */
+public class JLetter
+{
+    /**
+    * Prints letter "J".
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args)
+   {
+      //prints a line of JAVA.
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ Project-1/MyLifeGoals.java
@@ -0,0 +1,34 @@
+/** 
+ * Prints life goals of author.
+ *
+ *Project 1
+ * @author Kaffeein Bellamy
+ * @version 26/8/2019
+ */
+public class MyLifeGoals
+{
+   /**
+    * Prints life goals.
+    *
+    * @param args Command line arguments (not used).
+    */
+   public static void main(String[] args) 
+   {
+      // prints name
+      System.out.println("Kaffeein Bellamy");
+      System.out.println("");
+      // prints short-term goal
+      // note to self: The grader does NOT count spaces as characters.
+      System.out.println("I personally would like to "
+         + "graduate school in 4 years and have a well-paying job offer " 
+         + "at a large corporation waiting for me.");
+      // prints medium-term goal
+      System.out.println("I also would like to be a software " 
+         + "developer that works on programs for graphic designers "
+         + "and animators in the arts industry.");
+      // prints long-term goal 
+      System.out.println("Lastly, I would like to own a company that "
+         + "specializes in computer-generated objects and the "
+         + " animation of motion pictures.");
+   }
+}
